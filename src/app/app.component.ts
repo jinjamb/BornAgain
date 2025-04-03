2223c63bdafd31572ecc81cd85be89f700a2132a
jinjamb <jinjamb09@gmail.com>
2025-04-03 11:53:53 +0200

    jusqu'à page 82

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule, MatIconButton } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { AssignmentsComponent } from './assignments/assignments.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ 
+    MatButtonModule, 
+    CommonModule,
+    MatDividerModule,
+    MatIconModule,
+    AssignmentsComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
